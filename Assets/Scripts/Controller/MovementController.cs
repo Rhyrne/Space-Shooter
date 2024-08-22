3ebb4e3b4075defd7064b2776caf54f6546368c2
Rhyrne <100185977+Rhyrne@users.noreply.github.com>
2024-08-22 23:12:22 +0300

    Codes are edited

---
+++ Assets/Scripts/Controller/MovementController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController
+{
+    public Vector3 DriveShip(float moveHorizontal, float moveVertical, int speed)
+    {
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        return movement * speed;
+    }
+
+    public Quaternion TiltShip(Rigidbody physic, int rotationSpeed)
+    {
+        Quaternion rotation = Quaternion.Euler(0, 0, physic.velocity.x * rotationSpeed);
+
+        return rotation;
+    }
+}
---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Boundary
+{
+    private float xMin, xMax, zMin, zMax;
+
+    public Boundary(float xmin, float xmax, float zmin, float zmax) 
+    {
+        this.xMin = xmin;
+        this.xMax = xmax;
+        this.zMin = zmin;
+        this.zMax = zmax;
+    }
+
+    public Vector3 GetPosition(Rigidbody physic)
+    {
+        Vector3 position = new Vector3(
+            Mathf.Clamp(physic.position.x, xMin, xMax),
+            0,
+            Mathf.Clamp(physic.position.z, zMin, zMax)
+        );
+
+       return position;
+    }
+}
+
+public class PlayerController : MonoBehaviour
+{
+    // Değişkenler
+    private Rigidbody physic;
+    [SerializeField] private int speed;
+    [SerializeField] private int rotationSpeed;
+
+    [SerializeField] private float xMin;
+    [SerializeField] private float xMax;
+    [SerializeField] private float zMin;
+    [SerializeField] private float zMax;
+
+    //Sınıflar
+    public Boundary boundary ;
+    public MovementController movementController;
+
+    void Start()
+    {
+        physic = GetComponent<Rigidbody>();
+
+        // Sınıflardan nesne oluşturma
+        boundary = new Boundary(xMin, xMax, zMin, zMax);
+        movementController = new MovementController();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        physic.velocity = movementController.DriveShip(moveHorizontal, moveVertical, speed);
+
+        Vector3 position = boundary.GetPosition(physic);
+
+        physic.position = position;
+
+        physic.rotation = movementController.TiltShip(physic,rotationSpeed);
+
+    }
+}
