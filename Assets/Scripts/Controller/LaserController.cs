db59c807f1096eccf5cf897ea03a288807cda22e
Rhyrne <100185977+Rhyrne@users.noreply.github.com>
2024-08-27 23:09:32 +0300

    Laser added to ships

---
+++ Assets/Scripts/Controller/LaserController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+
+    Rigidbody physic;
+
+    [SerializeField] private float laserSpeed; 
+
+    void Start()
+    {
+        physic = GetComponent<Rigidbody>();
+
+        physic.velocity = transform.forward * laserSpeed;
+    }
+}
